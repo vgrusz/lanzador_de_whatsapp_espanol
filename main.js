08c56eeb69a4115d4373f9bb9c7bdb73370ab26a
victor <vgrusz@yahoo.com.ar>
2021-12-06 23:37:37 +0100

    primer commit 2bis

--- main.js
+++ main.js
@@ -1,83 +1,70 @@
 function lanzar() {
-    let prefijoNacional = document.getElementById("prefijoNacional").value;
+  let prefijoNacional = document.getElementById("prefijoNacional").value;
 
-    let telefono = document.getElementById("telefono").value;
+  let telefono = document.getElementById("telefono").value;
 
-    let telefonoSinEspacios = telefono.split(/\s/).join("");
+  let telefonoSinEspacios = telefono.split(/\s/).join("");
 
-    let primerTextoConstante =
-      document.getElementById("primerTextoConstante").value;
+  let primerTextoConstante = document.getElementById("primerTextoConstante").value;
 
-    let primerTextoVariable =
-      document.getElementById("primerTextoVariable").value;
+  let primerTextoVariable = document.getElementById("primerTextoVariable").value;
 
-    let segundoTextoConstante =
-      document.getElementById("segundoTextoConstante").value;
-    let segundoTextoVariable =
-      document.getElementById("segundoTextoVariable").value;
-    let ultimoTextoConstante = document.getElementById("ultimoTextoConstante").value;
+  let segundoTextoConstante = document.getElementById("segundoTextoConstante").value;
 
-    document.getElementById("telefono").value = "";
-    document.getElementById("primerTextoVariable").value = "";
-    document.getElementById("segundoTextoVariable").value = "";
+  let segundoTextoVariable = document.getElementById("segundoTextoVariable").value;
 
-    let url =
-      "https://api.whats"+"app.com/send/?phone=" +
-      prefijoNacional +
-      telefonoSinEspacios +
-      "&text=" +
-      primerTextoConstante +
-      " "+
-      primerTextoVariable +
-      " "+
-      segundoTextoConstante +
-      " "+
-      segundoTextoVariable +
-      " "+
-      ultimoTextoConstante +
-      "&app_absent=0";
+  let ultimoTextoConstante = document.getElementById("ultimoTextoConstante").value;
 
-    window.open(url);
-  }
+  document.getElementById("telefono").value = "";
+  document.getElementById("primerTextoVariable").value = "";
+  document.getElementById("segundoTextoVariable").value = "";
 
-  function guardarTextosConstantes() {
-    localStorage.setItem(
-      "prefijoNacional",
-      document.getElementById("prefijoNacional").value
-    );
-    localStorage.setItem(
-      "primerTextoConstante",
-      document.getElementById("primerTextoConstante").value
-    );
-    localStorage.setItem(
-      "segundoTextoConstante",
-      document.getElementById("segundoTextoConstante").value
-    );
-    localStorage.setItem(
-      "ultimoTextoConstante",
-      document.getElementById("ultimoTextoConstante").value
-    );
-  }
+  let tieneApp = document.getElementById("radioSi").checked;
 
-  function blanquearTextosConstantes() {
-    document.getElementById("prefijoNacional").value = "";
-    document.getElementById("primerTextoConstante").value = "";
-    document.getElementById("segundoTextoConstante").value = "";
-    document.getElementById("ultimoTextoConstante").value = "";
+  let appAbsent = tieneApp ? 0 : 1;
+  let url =
+    "https://api.whats" +
+    "app.com/send/?phone=" +
+    prefijoNacional +
+    telefonoSinEspacios +
+    "&text=" +
+    primerTextoConstante +
+    " " +
+    primerTextoVariable +
+    " " +
+    segundoTextoConstante +
+    " " +
+    segundoTextoVariable +
+    " " +
+    ultimoTextoConstante +
+    "&app_absent=" +
+    appAbsent;
 
-    localStorage.setItem("prefijoNacional", "");
-    localStorage.setItem("primerTextoConstante", "");
-    localStorage.setItem("segundoTextoConstante", "");
-    localStorage.setItem("ultimoTextoConstante", "");
-  }
+  window.open(url);
+}
 
-  function appLoad() {
-    document.getElementById("prefijoNacional").value =
-      localStorage.getItem("prefijoNacional");
-    document.getElementById("primerTextoConstante").value =
-      localStorage.getItem("primerTextoConstante");
-    document.getElementById("segundoTextoConstante").value =
-      localStorage.getItem("segundoTextoConstante");
-    document.getElementById("ultimoTextoConstante").value =
-      localStorage.getItem("ultimoTextoConstante");
-  }
+function guardarTextosConstantes() {
+  localStorage.setItem("prefijoNacional", document.getElementById("prefijoNacional").value);
+  localStorage.setItem("primerTextoConstante", document.getElementById("primerTextoConstante").value);
+  localStorage.setItem("segundoTextoConstante", document.getElementById("segundoTextoConstante").value);
+  localStorage.setItem("ultimoTextoConstante", document.getElementById("ultimoTextoConstante").value);
+}
+
+function blanquearTextosConstantes() {
+  document.getElementById("prefijoNacional").value = "";
+  document.getElementById("primerTextoConstante").value = "";
+  document.getElementById("segundoTextoConstante").value = "";
+  document.getElementById("ultimoTextoConstante").value = "";
+
+  localStorage.setItem("prefijoNacional", "");
+  localStorage.setItem("primerTextoConstante", "");
+  localStorage.setItem("segundoTextoConstante", "");
+  localStorage.setItem("ultimoTextoConstante", "");
+}
+
+function appLoad() {
+  document.getElementById("prefijoNacional").value = localStorage.getItem("prefijoNacional");
+  document.getElementById("primerTextoConstante").value = localStorage.getItem("primerTextoConstante");
+  document.getElementById("segundoTextoConstante").value = localStorage.getItem("segundoTextoConstante");
+  document.getElementById("ultimoTextoConstante").value = localStorage.getItem("ultimoTextoConstante");
+}
